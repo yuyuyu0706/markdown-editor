0c3bea16c980cf6b82b25470b7b6520297d546ee
yuyuyu0706 <37810641+yuyuyu0706@users.noreply.github.com>
2025-09-15 07:53:27 +0900

    fix: display mermaid node text

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ if (window.mermaid) {
   mermaid.initialize({
     startOnLoad: false,
     securityLevel: 'loose',
-    flowchart: { htmlLabels: true, wrap: true }
+    flowchart: { htmlLabels: false, wrap: true, useMaxWidth: false }
   });
 }
 
